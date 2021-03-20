c7b097e138b96f5783621b5d896c65a0c37b11e2
ZomboCode <75898681+ZomboCode@users.noreply.github.com>
2021-03-20 15:35:13 +0200

    Create userscript.js

---
+++ userscript.js
@@ -0,0 +1,12 @@
+/ ==UserScript==
+// @name         Built-in specific property editor
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  For working with massive trigger circuits
+// @author       ZomboCom
+// @match        https://krunker.io/editor.html
+// @require 
+// @grant        none
+// ==/UserScript==
+
+mod();
